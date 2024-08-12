39656d475d17ce214b2f717d11275e071cfd3692
Krishnaprasad M <krishnaprasad.m3@gmail.com>
2024-08-12 10:19:17 +0530

    commit-10th August

---
+++ src/excercise10082024/Lab001.py
@@ -0,0 +1,26 @@
+#checking comment out option
+#----------------------------------------------------------------------------------------
+# Concept of sep and end
+# print("Hello","Krishna","August",2,2+2,"2",sep="+");
+# print("Hello","Krishna","August",2,2+2,"2");
+# print("Hello","Krishna","August",2,2+2,"2",sep="+",end="_");
+# print("Amit")
+#-------------------------------------------------------------------------------------------
+# Maximum of numbers
+# print(max(10,18))
+# print(max(12,15,2,35))
+# #---------------------------------------------------------------------------------------------
+# #Keywords
+# import keyword
+# print(keyword.kwlist)
+#------------------------------------------------------------------------------------
+#variables
+a=10
+b=21
+c=32
+sum = a+b+c
+sum = sum +11
+sum = sum-1
+print(sum)
+x,y,z=10,12,15
+print(x,y,z,sep="_")
