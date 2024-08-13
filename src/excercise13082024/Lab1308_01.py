29c8bee5a0c12be6f1ccaec04b6f13352027ce76
Krishnaprasad M <krishnaprasad.m3@gmail.com>
2024-08-13 14:46:55 +0530

    commit-13th August

---
+++ src/excercise13082024/Lab1308_01.py
@@ -0,0 +1,41 @@
+#13th Aug 2024
+#1.Multicomment
+"""
+whatever statements we write inside these triple quotes (") are considered commented.
+we use this technique for commenting  multiple lines
+"""
+age = 65
+# here age is variable name or identifier
+# = is Assignment operator
+# 65 is variable value or literal
+
+a= pow(2,3)
+print(a)
+b = abs(-6)
+print(b)
+
+is_lie = True
+print(is_lie)
+
+"""#Taking user input
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
+print("The Sum is ",num1 + num2)
+print("The Difference is ",num1 - num2)
+print("The multiple is ",num1 * num2)
+print("The Divisor is ",num1 / num2)
+"""
+
+#String formating
+n=3.14567832
+f_num= f"{n:.4f}"
+print(f_num)
+
+val=50
+print("The number is "f"{val}")
+
+name = "Krishna"
+print(name.upper())
+print(name.lower())
+print(type(name))
+print(len(name))
