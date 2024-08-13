5238fba2b858bb36a04e33b78b9a8a1858039d9a
Krishnaprasad M <krishnaprasad.m3@gmail.com>
2024-08-13 14:46:55 +0530

    commit-13th August

---
+++ src/excercise13082024/HW_13AUG.py
@@ -0,0 +1,23 @@
+#Take the input from user and print the table
+n=int(input("Enter the number which you want to print the table: "))
+print(f"{n}"" * 1 = "f"{n*1}")
+print(f"{n}"" * 2 = "f"{n*2}")
+print(f"{n}"" * 3 = "f"{n*3}")
+print(f"{n}"" * 4 = "f"{n*4}")
+print(f"{n}"" * 5 = "f"{n*5}")
+print(f"{n}"" * 6 = "f"{n*6}")
+print(f"{n}"" * 7 = "f"{n*7}")
+print(f"{n}"" * 8 = "f"{n*8}")
+print(f"{n}"" * 9 = "f"{n*9}")
+print(f"{n}"" * 10 = "f"{n*10}")
+
+#Take 2 inputs from user and find max,pow,sum,dif,mult,div
+
+num1 =  int(input("Enter the First number: "))
+num2 = int(input("Enter the Second number: "))
+print("The Maximum number between "f"{num1}" " and "f"{num2}"" is ", max(num1,num2))
+print("The power of "f"{num1}" " to "f"{num2}"" is ", pow(num1,num2))
+print("The Sum of  "f"{num1}" " and "f"{num2}"" is ", num1 + num2)
+print("The Difference of  "f"{num1}" " and "f"{num2}"" is ", num1 - num2)
+print("The Multiple of  "f"{num1}" " and "f"{num2}"" is ", num1 * num2)
+print("The Quotient of  "f"{num1}" " and "f"{num2}"" is ", num1 / num2)
